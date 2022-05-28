d9a2dcfb08342814b2d0885fc2e858fa461ec7d3
Deepesh <deepesh99.nair@gmail.com>
2022-05-28 17:14:02 +0530

    + login module was added with jwt + code optimizaton in controller/login.js + /login route added

--- controller/login.js
+++ controller/login.js
@@ -1,4 +1,5 @@
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 
 const UserAuth = require('../model/userAuth');
 
@@ -13,18 +14,60 @@ const UserAuth = require('../model/userAuth');
  * @returns json - if user exist then status as false else true.
  */
 exports.register = async (req, res) => {
-  const email = await UserAuth.findOne({ where: { email: req.body.email } });
+  const { email, password } = req.body;
 
-  if (email) {
-    return res.status(401).json({ status: false, message: 'User already exists in databse' });
+  try {
+    // Checks in database if email exists
+    const user = await UserAuth.findOne({ where: { email } });
+
+    // if email exist then return fail message
+    if (user) {
+      return res.status(401).json({ status: false, message: 'User already exists in databse' });
+    }
+
+    // creating a salt for hashed password. change number for complexity
+    const salt = await bcrypt.genSalt(11);
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+    // email and hashed password is stored in database
+    await UserAuth.create({
+      email,
+      password: hashedPassword,
+    });
+
+    return res.status(200).json({ status: true, message: 'Registration Success' });
+  } catch (err) {
+    return res.status(401).json({ status: false, message: 'Server Error' });
   }
+};
 
-  const salt = await bcrypt.genSalt(11);
-  const hashedPassword = await bcrypt.hash(req.body.password, salt);
-  await UserAuth.create({
-    email: req.body.email,
-    password: hashedPassword,
-  });
+/**
+ * This function takes user email and password checks againts database
+ * @param {*} req
+ * @param {email} req.email - email given by user on login
+ * @param {password} req.password - password given by user on login
+ * @param {*} res
+ * @returns json - with jwt token and status message
+ */
+exports.login = async (req, res) => {
+  const { email, password } = req.body;
 
-  return res.status(200).json({ status: true, message: 'Registration Success' });
+  try {
+    const user = await UserAuth.findOne({ where: { email } });
+    const validUser = await bcrypt.compare(password, user.password);
+
+    if (user && validUser) {
+      // create jwt tokens
+      // scret is used to create hash. TODO: encrypt secret
+      const secret = 'thisisjwttoken12345';
+
+      // token will have userid and expire in 30days
+      const token = jwt.sign({ userid: user.id }, secret, { expiresIn: '7 days' });
+      return res.status(200).json({ status: true, message: 'Login Success!!', token });
+    }
+
+    return res.status(401).json({ status: false, message: 'Email or Password is incorrect' });
+  } catch (err) {
+    return res.status(401).json({ status: false, message: 'Server Error' });
+  }
 };
--- routes/general.js
+++ routes/general.js
@@ -1,10 +1,11 @@
 const express = require('express');
 
-const { register } = require('../controller/login');
+const { register, login } = require('../controller/login');
 
 const router = express.Router();
 
-// router.get('/login', );
+// login route
+router.post('/login', login);
 
 // This route is to register new user into database.
 router.post('/register', register);
