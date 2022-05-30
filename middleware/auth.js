1d4db01c60d4d83270b553cd91ea1b76a697e150
Deepesh <deepesh99.nair@gmail.com>
2022-05-30 23:38:01 +0530

    + updated file with comments for understanding

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,13 +1,21 @@
 const jwt = require('jsonwebtoken');
-
+/**
+ * This functions verifies jwt token is correct or not expired
+ *  if true then next() else return error
+ * @param {*} req - use jwt token from headers
+ * @param {*} res
+ * @param {*} next - goes to next controller function from route
+ * @returns json - if error occurs
+ */
 exports.authorization = (req, res, next) => {
+  // TODO: encrypt secret
   const secret = 'thisisjwttoken12345';
 
-  // TODO: verify jwt tokens and assign userID if correct
   try {
     const authToken = req.headers.token;
     const tokenData = jwt.verify(authToken, secret);
 
+    // exp data from token is in UNIX timestanp so need to convert Date.now to same format.
     if (tokenData.exp < Math.floor(Date.now() / 1000)) {
       return res.status(401).json({ status: false, message: 'Login expired' });
     }
