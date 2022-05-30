e319db36d56c8d2565e9367d4d781b085f1bd80e
Deepesh <deepesh99.nair@gmail.com>
2022-05-30 23:31:15 +0530

    + updated auth middleware to verify jwt tokens

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,5 +1,19 @@
+const jwt = require('jsonwebtoken');
+
 exports.authorization = (req, res, next) => {
+  const secret = 'thisisjwttoken12345';
+
   // TODO: verify jwt tokens and assign userID if correct
-  res.locals.userId = req.params.userId;
-  next();
+  try {
+    const authToken = req.headers.token;
+    const tokenData = jwt.verify(authToken, secret);
+
+    if (tokenData.exp < Math.floor(Date.now() / 1000)) {
+      return res.status(401).json({ status: false, message: 'Login expired' });
+    }
+    res.locals.userId = tokenData.userid;
+    return next();
+  } catch (err) {
+    return res.status(401).json({ status: false, message: `${err.name}` });
+  }
 };
